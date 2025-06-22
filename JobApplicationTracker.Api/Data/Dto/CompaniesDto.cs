174ba53da72b423ad14170929094de0b480cd015
Prashiddhika Bhattarai <bhattaraipracdka@gmail.com>
2025-06-22 10:46:47 +0545

    companies api completed

---
+++ JobApplicationTracker.Api/Data/Dto/CompaniesDto.cs
@@ -0,0 +1,16 @@
+namespace JobApplicationTracker.Api.Data.Dto;
+
+public class CompaniesDto
+{
+    public int CompanyId { get; set; }
+    public string CompanyName { get; set; }
+    public string CompanyLogo { get; set; }
+    public int? IndustryId { get; set; } // Nullable to allow for optional industry association
+    public int? CompanySizeId { get; set; } // Nullable to allow for optional company size
+    public string Website { get; set; }
+    public string Location { get; set; }
+    public string Description { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime UpdatedAt { get; set; }
+}
+
